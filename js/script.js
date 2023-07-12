454d045492b6192f39cb346232c5c6feb7227eae
Dmitriy Nikolayev <nikolayevdima88@gmail.com>
2023-07-13 00:28:38 +0600

    Fix

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@
 let jupiter = {
     selector: `jupiter`,
     obj: document.querySelector('#jupiter-orbit div'),
-    radius: 200,
+    orbit: 200,
     speed: 0.002,
-    width: 10,
+    radius: 5,
     radian: 0
 }
 
@@ -24,10 +24,13 @@ planets.forEach((item, key) => {
     let pd = document.querySelector(`#${item.selector}-orbit-pd`);
     let zindex = (key + 1) * 10
 
-    planetOrbit.style.top = parseInt(400 - item.radius) + 'px'
-    planetOrbit.style.left = parseInt(400 - item.radius) + 'px'
-    planetOrbit.style.width = parseInt(item.radius * 2) + 'px'
-    planetOrbit.style.height = parseInt(item.radius * 2) + 'px'
+    planet.style.width = parseInt(1 + item.radius*2) + 'px';
+    planet.style.height = parseInt(1 + item.radius*2) + 'px';
+
+    planetOrbit.style.top = parseInt(400 - item.orbit) + 'px'
+    planetOrbit.style.left = parseInt(400 - item.orbit) + 'px'
+    planetOrbit.style.width = parseInt(item.orbit * 2 + 1) + 'px'
+    planetOrbit.style.height = parseInt(item.orbit * 2 + 1) + 'px'
     //planet.style['z-index'] = zindex
 
     pd.style.top = parseInt(400 - item.radius + 15) + 'px'
@@ -40,11 +43,12 @@ planets.forEach((item, key) => {
 setInterval(() => {
     planets.forEach((item) => {
         let planet = document.querySelector(`#${item.selector}-orbit div`);
+
         item.radian += item.speed
-        planet.style.left = item.radius + item.radius * Math.cos(item.radian) - item.width/2 + 'px'
-        planet.style.top = item.radius - item.radius * Math.sin(item.radian) - item.width/2 + 'px'
+        planet.style.left = parseInt(item.orbit + 1 + item.orbit * Math.cos(item.radian) - item.radius) + 'px'
+        planet.style.top =  parseInt(item.orbit + 1 + item.orbit * Math.sin(item.radian) - item.radius) + 'px'
     })
-}, 5)
+}, 1)
 
 //let marsOrbit = document.querySelector('#mars-orbit');
 let jupiterOrbit = document.querySelector('#jupiter-orbit');
